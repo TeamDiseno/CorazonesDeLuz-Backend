6428f62c211cc1f381e6d7c93238259a23bbf179
Marceloalvarez12 <71199245+Marceloalvarez12@users.noreply.github.com>
2021-03-16 00:35:27 -0300

    Revert "Registrar niño"

--- src/main/java/com/database/repository/IRepositoryNiño.java
+++
@@ -1,9 +0,0 @@
-package com.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.domain.entity.*;
-
-public interface IRepositoryNiño extends JpaRepository<niño,Integer>{
-
-}
--- src/test/java/com/fundacion/demo/ApplicationTests.java
+++ src/test/java/com/fundacion/demo/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.registrarUsuario.demo;
+package com.fundacion.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
